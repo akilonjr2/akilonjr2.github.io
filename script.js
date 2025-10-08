6c6cf48625a5092013d31535358d3fb9215b36b8
bjorn.smitt <bjorn.smitt@hva.nl>
2025-10-08 13:24:24 +0200

    first version

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form')?.addEventListener('submit', (event) => {
+    event.preventDefault();
+    alert('The Court has received your message... We are watching.');
+});
